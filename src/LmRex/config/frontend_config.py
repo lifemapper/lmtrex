d9db1d1bcc94a069604691bf6e6f9d957a13b2da
Maksym Patiiuk <40512816+maxxxxxdlp@users.noreply.github.com>
2021-03-16 19:21:14 +0200

    Remove OpenAPI testing and UI code

--- src/LmRex/config/frontend_config.py
+++ src/LmRex/config/frontend_config.py
@@ -3,13 +3,7 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
-FRONTEND_BASE_DIR = os.path.join(BASE_DIR, '../frontend')
-
-TEMPLATES_DIR = os.path.join(BASE_DIR, '../frontend/static/templates')
-
-OPEN_API_LOCATION = os.path.join(BASE_DIR, 'open_api.yaml')
-
-ERROR_LOGS_LOCATION = os.path.join(BASE_DIR, '../automated_testing/error_logs')
+TEMPLATES_DIR = os.path.join(BASE_DIR, '../collection_stats/static/templates')
 
 COLLECTION_STATS_BASE_DIR = os.path.join(BASE_DIR, '../collection_stats')
 
