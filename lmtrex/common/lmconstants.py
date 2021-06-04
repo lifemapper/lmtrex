7d7dedce39dbc623ae06b4529e62e310e65a0bb3
zzeppozz <zzeppozz@gmail.com>
2021-06-04 15:12:14 -0500

    constants and methods to support view and api URLs

--- lmtrex/common/lmconstants.py
+++ lmtrex/common/lmconstants.py
@@ -1,6 +1,5 @@
 import os
 from lmtrex.services.api.v1.s2n_type import S2nKey
-from ctypes.test import test_returnfuncptrs
 
 # .............................................................................
 # hierarchySoFarWRanks <class 'list'>: ['41107:$Kingdom:Plantae$Subkingdom:Viridiplantae$Infrakingdom:Streptophyta$Superdivision:Embryophyta$Division:Tracheophyta$Subdivision:Spermatophytina$Class:Magnoliopsida$Superorder:Lilianae$Order:Poales$Family:Poaceae$Genus:Poa$Species:Poa annua$']
@@ -18,6 +17,8 @@ SPECIFY_ARK_PREFIX = 'http://spcoco.org/ark:/'
 
 DATA_DUMP_DELIMITER = '\t'
 GBIF_MISSING_KEY = 'unmatched_gbif_ids'
+
+ICON_OPTIONS = ('active', 'inactive', 'hover')
     
 # .............................................................................
 class DWC:
@@ -124,28 +125,48 @@ class APIService:
     Name = 'name'
     Map = 'map'
     Heartbeat = 'heartbeat'
+    # Specify guid resolver
     Resolve = 'resolve'
+    # Direct access to syftorium upload
     Address = 'address'
+    # Icons for service providers
+    Badge = 'badge'
 
+ICON_DIR='lmtrex/static/img'
 # .............................................................................
 class ServiceProvider:
-    BISON = {
-        S2nKey.NAME: 'BISON', S2nKey.PARAM: 'bison', S2nKey.SERVICES: [APIService.Occurrence]}
     GBIF = {
         S2nKey.NAME: 'GBIF', S2nKey.PARAM: 'gbif', S2nKey.SERVICES: [
-            APIService.Occurrence, APIService.Name]}
+            APIService.Occurrence, APIService.Name, APIService.Badge],
+        'icon': {'active': '{}/gbif_active-01.png'.format(ICON_DIR),
+                 'inactive': '{}/gbif_inactive-01.png'.format(ICON_DIR),
+                 'hover': '{}/gbif_hover-01-01.png'.format(ICON_DIR)}
+        }
     iDigBio = {
         S2nKey.NAME: 'iDigBio', S2nKey.PARAM: 'idb', S2nKey.SERVICES: [
-            APIService.Occurrence]}
+            APIService.Occurrence, APIService.Badge],
+        'icon': {'active': '{}/idigbio_colors_active-01.png'.format(ICON_DIR),
+                 'inactive': '{}/idigbio_colors_inactive-01.png'.format(ICON_DIR),
+                 'hover': '{}/idigbio_colors_hover-01.png'.format(ICON_DIR)}
+        }
+    # TODO: need an ITIS badge
     ITISSolr = {
         S2nKey.NAME: 'ITIS', S2nKey.PARAM: 'itis', S2nKey.SERVICES: [
             APIService.Name]}
     Lifemapper = {
         S2nKey.NAME: 'Lifemapper', S2nKey.PARAM: 'lm', S2nKey.SERVICES: [
-            APIService.Map]}
+            APIService.Map, APIService.Badge],
+        'icon': {'active': '{}/lm_active.png'.format(ICON_DIR),
+                 'inactive': '{}/lm_inactive-01.png'.format(ICON_DIR),
+                 'hover': '{}/lm_hover-01.png'.format(ICON_DIR)}
+        }
     MorphoSource = {
         S2nKey.NAME: 'MorphoSource', S2nKey.PARAM: 'mopho', S2nKey.SERVICES: [
-            APIService.SpecimenExtension, APIService.Occurrence]}
+            APIService.SpecimenExtension, APIService.Occurrence, APIService.Badge],
+        'icon': {'active': '{}/morpho_active-01.png'.format(ICON_DIR),
+                 'inactive': '{}/morpho_inactive-01.png'.format(ICON_DIR),
+                 'hover': '{}/morpho_hover-01.png'.format(ICON_DIR)}
+        }
     Specify = {
         S2nKey.NAME: 'Specify', S2nKey.PARAM: 'specify', S2nKey.SERVICES: [
             APIService.Occurrence, APIService.Resolve]}
@@ -305,8 +326,8 @@ class BISON:
     
     LIMIT = 1000
     # Ends in : to allow appending unique id
-    LINK_PREFIX = ('https://bison.usgs.gov/solr/occurrences/select/' +
-                   '?q=occurrenceID:')
+    # LINK_PREFIX = ('https://bison.usgs.gov/solr/occurrences/select/' +
+    #                '?q=occurrenceID:')
     LINK_FIELD = 'bisonurl'
     # For TSN query filtering on Binomial
     NAME_KEY = 'ITISscientificName'
@@ -426,8 +447,12 @@ class Lifemapper:
 
 # ......................................................
 class MorphoSource:
-    URL = 'https://ms1.morphosource.org/api/v1'
-    FROZEN_URL = 'https://ea-boyerlab-morphosource-01.oit.duke.edu/api/v1'
+    REST_URL = 'https://ms1.morphosource.org/api/v1'
+    VIEW_URL = 'https://www.morphosource.org/concern/biological_specimens'
+    # VIEW_URL = 'https://www.morphosource.org/concern/biological_specimens/000S31459?locale=en'
+    # FROZEN_URL = 'https://ea-boyerlab-morphosource-01.oit.duke.edu/api/v1'
+    DWC_ID_FIELD = 'occurrence_id'
+    LOCAL_ID_FIELD = 'id'
     OCC_RESOURCE = 'specimens'
     MEDIA_RESOURCE = 'media'
     OTHER_RESOURCES = ['taxonomy', 'projects', 'facilities']
@@ -437,6 +462,15 @@ class MorphoSource:
     RECORDS_KEY = 'results'
     LIMIT = 1000
     RECORD_FORMAT = 'https://www.morphosource.org/About/API'
+
+    @classmethod
+    def get_occurrence_view(cls, local_id):
+        return '{}/000S{}'.format(MorphoSource.VIEW_URL, local_id)
+
+    @classmethod
+    def get_occurrence_data(cls, occurrence_id):
+        return '{}/find/specimens?start=0&limit=1000&q={}%3A{}'.format(
+            MorphoSource.REST_URL, MorphoSource.LOCAL_ID_FIELD, occurrence_id)
     
 # ......................................................
 class SPECIFY:
@@ -454,11 +488,12 @@ class GBIF:
     TAXON_KEY = 'specieskey'
     TAXON_NAME = 'sciname'
     PROVIDER = 'puborgkey'
-    GBIFID = 'gbifid'
+    OCC_ID_FIELD = 'gbifID'
+    SPECIES_ID_FIELD = 'key'
     WAIT_TIME = 180
     LIMIT = 300
-    REST_URL = 'http://api.gbif.org/v1'
-    SPECIES_URL = 'https://www.gbif.org/species'
+    VIEW_URL = 'https://www.gbif.org'
+    REST_URL = 'https://api.gbif.org/v1'
     QUALIFIER = 'gbif:'
 
     SPECIES_SERVICE = 'species'
@@ -509,7 +544,27 @@ class GBIF:
     # We are adding the 2 fields: LM_WKT_FIELD and LINK_FIELD
     LINK_FIELD = 'gbifurl'
     # Ends in / to allow appending unique id
-    LINK_PREFIX = 'http://www.gbif.org/occurrence/'
+    # LINK_PREFIX = 'http://www.gbif.org/occurrence/'
+    
+    @classmethod
+    def species_url(cls):
+        return '{}/{}'.format(GBIF.VIEW_URL, GBIF.SPECIES_SERVICE)
+    
+    @classmethod
+    def get_occurrence_view(cls, key):
+        return '{}/{}/{}'.format(GBIF.VIEW_URL, GBIF.OCCURRENCE_SERVICE, key)
+
+    @classmethod
+    def get_occurrence_data(cls, key):
+        return '{}/{}/{}'.format(GBIF.REST_URL, GBIF.OCCURRENCE_SERVICE, key)
+
+    @classmethod
+    def get_species_view(cls, key):
+        return '{}/{}/{}'.format(GBIF.VIEW_URL, GBIF.SPECIES_SERVICE, key)
+
+    @classmethod
+    def get_species_data(cls, key):
+        return '{}/{}/{}'.format(GBIF.REST_URL, GBIF.SPECIES_SERVICE, key)
 
 
 # .............................................................................
@@ -518,7 +573,7 @@ class COMMUNITY_SCHEMA:
     GBIF = {'code': 'gbif', 'url': 'https://gbif.github.io/dwc-api/apidocs/org/gbif/dwc/terms/GbifTerm.html'}
     DCT = {'code': 'dcterms', 'url': 'http://purl.org/dc/terms'}
     IDB = {'code': 'idigbio', 'url': ''}
-    MS = {'code': 'mopho', 'url': ''}
+    MS = {'code': 'mopho', 'url': 'https://www.morphosource.org/About/API'}
     S2N = {'code': 's2n', 'url': ''}
 
 # .............................................................................
@@ -528,6 +583,9 @@ class S2N_SCHEMA:
         All field values are strings unless otherwise indicated
     """
     NAME = {
+        # Provider's URLs to this record
+        'view_url': COMMUNITY_SCHEMA.S2N,
+        'api_url': COMMUNITY_SCHEMA.S2N,
         # S2n standardization of common elements
         'status': COMMUNITY_SCHEMA.S2N,
         'scientific_name': COMMUNITY_SCHEMA.S2N,
@@ -552,6 +610,10 @@ class S2N_SCHEMA:
                                                     #     each dict will also contain 'tsn': <tsn>
         }
     MAP = {
+        # Provider's URLs to this record
+        'view_url': COMMUNITY_SCHEMA.S2N,
+        'api_url': COMMUNITY_SCHEMA.S2N,
+
         'endpoint': COMMUNITY_SCHEMA.S2N,
         'data_link': COMMUNITY_SCHEMA.S2N,
         'sdm_projection_scenario_code': COMMUNITY_SCHEMA.S2N,
@@ -569,9 +631,12 @@ class S2N_SCHEMA:
         'vendor_specific_parameters': COMMUNITY_SCHEMA.S2N,     # dictionary with queryparameter/value
         }
     OCCURRENCE = {
+        # Provider's URLs to this record
+        'view_url': COMMUNITY_SCHEMA.S2N,
+        'api_url': COMMUNITY_SCHEMA.S2N,
         # S2n resolution of non-standard contents
-        'idigbio_flags': COMMUNITY_SCHEMA.S2N,      # list of strings
-        'gbif_issues': COMMUNITY_SCHEMA.S2N,        # list of strings
+        'idigbio_flags': COMMUNITY_SCHEMA.S2N,      # dictionary of codes: descriptions
+        'gbif_issues': COMMUNITY_SCHEMA.S2N,        # dictionary of codes: descriptions
 
         # GBIF-specific field
         'gbifID': COMMUNITY_SCHEMA.GBIF,
@@ -579,6 +644,9 @@ class S2N_SCHEMA:
 
         # iDigBio-specific field
         'uuid': COMMUNITY_SCHEMA.IDB,
+        
+        # MorphoSource-specific field
+        'specimen_id': COMMUNITY_SCHEMA.MS,
 
         'accessRights': COMMUNITY_SCHEMA.DCT,
         'language': COMMUNITY_SCHEMA.DCT,
@@ -646,6 +714,9 @@ class S2N_SCHEMA:
             std_name = '{}:{}'.format(comschem['code'], fn)
             if fn == 'gbif_issues':
                 gname_stdname['issues'] = std_name
+            # # these will be computed with key
+            # elif fn in ('view_url', 'data_url'):
+            #     gname_stdname[fn] = std_name
             else:
                 gname_stdname[fn] = std_name
         return gname_stdname
@@ -657,6 +728,9 @@ class S2N_SCHEMA:
             stdname = '{}:{}'.format(comschem['code'], fn)
             if fn == 'idigbio_flags':
                 iname_stdname['flags'] = stdname
+            # # these will be computed with UUID
+            # elif fn in ('view_url', 'data_url'):
+            #     iname_stdname[fn] = stdname
             else:
                 iname_stdname[stdname] = stdname
         return iname_stdname
@@ -674,7 +748,9 @@ class S2N_SCHEMA:
     def get_mopho_occurrence_map(cls):
         dwc = COMMUNITY_SCHEMA.DWC['code']
         idb = COMMUNITY_SCHEMA.IDB['code']
+        s2n = COMMUNITY_SCHEMA.S2N['code']
         mapping = {
+            'specimen.specimen_id': '{}:view_url'.format(s2n),
             'specimen.catalog_number': '{}:catalogNumber'.format(dwc),
             'specimen.institution_code': '{}:institutionCode'.format(dwc),
             'specimen.occurrence_id': '{}:occurrenceID'.format(dwc), 
@@ -686,6 +762,7 @@ class S2N_SCHEMA:
     def get_gbif_name_map(cls):
         s2n = COMMUNITY_SCHEMA.S2N['code']
         mapping = {
+            'usageKey': '{}:provider_url'.format(s2n),
             'scientificName': '{}:scientific_name'.format(s2n),
             'canonicalName': '{}:canonical_name'.format(s2n), 
             'kingdom': '{}:kingdom'.format(s2n),
@@ -785,7 +862,9 @@ class Idigbio:
     """iDigBio constants enumeration"""
     NAMESPACE_URL = ''
     NAMESPACE_ABBR = 'gbif'
-    LINK_PREFIX = 'https://www.idigbio.org/portal/records/'
+    VIEW_URL = 'https://www.idigbio.org/portal/records'
+    REST_URL = 'https://search.idigbio.org/v2/view/records'
+    # LINK_PREFIX = 'https://www.idigbio.org/portal/records/'
     SEARCH_PREFIX = 'https://search.idigbio.org/v2'
     SEARCH_POSTFIX = 'search'
     COUNT_POSTFIX = 'summary/count'
@@ -809,6 +888,14 @@ class Idigbio:
     COUNT_KEY = 'itemCount'
     RECORDS_KEY = 'items'
     RECORD_FORMAT = 'https://github.com/idigbio/idigbio-search-api/wiki'
+    
+    @classmethod
+    def get_occurrence_view(cls, uuid):
+        return '{}/{}'.format(Idigbio.VIEW_URL, uuid)
+
+    @classmethod
+    def get_occurrence_data(cls, uuid):
+        return '{}/{}'.format(Idigbio.REST_URL, uuid)
     
 
 # .............................................................................
