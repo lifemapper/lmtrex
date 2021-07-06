0a6cd118833290d6fb2b3b206aaa289c627a310c
Maksym Patiiuk <40512816+maxxxxxdlp@users.noreply.github.com>
2021-07-07 01:16:21 +0300

    Update frontend endpoint due to provider schema changes

--- lmtrex/frontend/helpers.py
+++ lmtrex/frontend/helpers.py
@@ -1,14 +1,5 @@
-provider_icon_mapper = {
-    'Lifemapper': 'lm',
-    'MorphoSource': 'mopho',
-    'GBIF': 'gbif',
-    'iDigBio': 'idb',
-    'ITIS': '',
-    'Specify': '',
-}
-
 def provider_label_to_icon_url(provider_label):
     return (
         f'https://broker-dev.spcoco.org/api/v1/badge/?provider='
-        f'{provider_icon_mapper[provider_label]}'
-        f'&icon_status=active')
+        f'{provider_label}&icon_status=active'
+    )
