38cb36726030ae9c1f6be69f8c649d8ce31191b6
Max Patiiuk <max@patii.uk>
2021-09-21 08:27:49 -0500

    Add boilerplate node files

---
+++ lmtrex/frontend/js_src/webpack.config.js
@@ -0,0 +1,90 @@
+const path = require('path');
+const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
+
+module.exports = (_environment, argv) => ({
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|jpeg|svg)$/,
+        type: 'asset/inline',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.[jt]s$/,
+        exclude: /(node_modules)/,
+        use: [
+          {
+            loader: 'babel-loader?+cacheDirectory',
+            options: {
+              presets: [
+                [
+                  '@babel/preset-env',
+                  {
+                    useBuiltIns: 'usage',
+                    corejs: '3.15',
+                    bugfixes: true,
+                    browserslistEnv: argv.mode,
+                  },
+                ],
+                ['@babel/preset-typescript'],
+              ],
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.ts'],
+    symlinks: false,
+  },
+  plugins: [
+    new WebpackManifestPlugin({
+      fileName: 'manifest.json',
+    }),
+  ],
+  devtool: argv.mode === 'development' ? 'eval-source-map' : 'source-map',
+  entry: {
+    frontend: './lib/frontend/entry.ts',
+    stats: './lib/stats/entry.ts',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/static/js/',
+    filename:
+      argv.mode === 'development'
+        ? '[name].bundle.js'
+        : '[name].[contenthash].bundle.js',
+    clean: true,
+    libraryExport: 'default',
+    environment: {
+      arrowFunction: true,
+      const: true,
+      destructuring: true,
+      ...(argv.mode === 'development'
+        ? {
+            bigIntLiteral: true,
+            dynamicImport: true,
+            forOf: true,
+            module: true,
+          }
+        : {}),
+    },
+  },
+  watchOptions: {
+    ignored: '/node_modules/',
+  },
+  stats: {
+    env: true,
+    outputPath: true,
+    warnings: true,
+    errors: true,
+    errorDetails: true,
+    errorStack: true,
+    moduleTrace: true,
+    timings: true,
+  },
+});
