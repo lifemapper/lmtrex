9ebcdf3b31f3b6f034087d2e8e7d0111aad0c2c0
Max Patiiuk <max@patii.uk>
2021-10-01 14:49:11 -0500

    Rewrite formatTable to JavaScript

---
+++ lmtrex/frontend/js_src/lib/frontend/formatTable.tsx
@@ -0,0 +1,76 @@
+import React from 'react';
+
+import type { Component, RA } from '../config';
+import { Row, Table } from './components';
+import type { BrokerRecord } from './entry';
+import { mapFields } from './fieldMapper';
+import { reorderFields } from './reorderFields';
+import { getRecordKeys, getValue } from './responseToTable';
+
+export function Response({
+  responses,
+}: {
+  responses: RA<BrokerRecord>;
+}): Component {
+  return (
+    <Table
+      className="data"
+      header={
+        <>
+          <td />
+          {responses.map(({ provider, record }) => (
+            <th key={provider.code} scope="col">
+              <div>
+                <img alt="" src={`${provider.icon_url}&icon_status=active`} />
+                <span>
+                  {provider.label}{' '}
+                  {typeof record['s2n:view_url'] === 'string' ? (
+                    <a
+                      href={record['s2n:view_url']}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      (link)
+                    </a>
+                  ) : (
+                    ''
+                  )}
+                </span>
+              </div>
+            </th>
+          ))}
+        </>
+      }
+    >
+      {mapFields(
+        reorderFields(
+          Object.fromEntries(
+            getRecordKeys(responses.map((response) => response.record)).map(
+              (key) =>
+                [
+                  key,
+                  responses.map((response) => getValue(response, key)),
+                ] as const
+            )
+          )
+        )
+      ).map(({ label, title, originalCells, cells }) => (
+        <Row
+          key={label}
+          header={label}
+          title={title}
+          className={
+            new Set(
+              originalCells
+                .filter((value) => value)
+                .map((value) => JSON.stringify(value))
+            ).size === 1
+              ? 'identical'
+              : ''
+          }
+          cells={cells}
+        />
+      ))}
+    </Table>
+  );
+}
