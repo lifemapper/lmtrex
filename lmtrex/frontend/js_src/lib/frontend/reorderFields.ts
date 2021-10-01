293f71f30709d018d23505312492684f7e2c36db
Max Patiiuk <max@patii.uk>
2021-10-01 14:51:12 -0500

    Rewrite reorderFields to JavaScript

---
+++ lmtrex/frontend/js_src/lib/frontend/reorderFields.ts
@@ -0,0 +1,30 @@
+import type { IR } from '../config';
+
+const fieldOrder: IR<{ readonly after: string }> = {
+  'gbif:acceptedScientificName': { after: 'dwc:scientificName' },
+  'dwc:datasetName': { after: 'dwc:specificEpithet' },
+};
+
+export function reorderFields<T>(mappedTable: IR<T>): IR<T> {
+  let reorderedTable = mappedTable;
+  Object.entries(fieldOrder)
+    .filter(
+      ([fieldName, position]) =>
+        !(fieldName in reorderedTable) ||
+        !('after' in position) ||
+        !(position.after in reorderedTable)
+    )
+    .forEach(([fieldName, { after }]) => {
+      const table = Object.entries(reorderedTable);
+      const keys = Object.keys(reorderedTable);
+      const currentPosition = keys.indexOf(fieldName);
+      let targetPosition = keys.indexOf(after);
+      const currentEntry = table.splice(currentPosition, 1)[0];
+      if (currentPosition > targetPosition) targetPosition += 1;
+
+      table.splice(targetPosition, 0, currentEntry);
+      reorderedTable = Object.fromEntries(table);
+    });
+
+  return reorderedTable;
+}
