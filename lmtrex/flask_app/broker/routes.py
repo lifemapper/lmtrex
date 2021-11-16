5c08b5529f59a689af4ee4dacdda34ff902c4024
zzeppozz <zzeppozz@gmail.com>
2021-11-16 15:01:00 -0600

    converted address and badge APIs to Flask; other APIs unfinished

--- lmtrex/flask_app/broker/routes.py
+++ lmtrex/flask_app/broker/routes.py
@@ -1,6 +1,8 @@
 from flask import Flask, Blueprint, request
 from markupsafe import escape
 
+from lmtrex.flask_app.broker.address import AddressSvc
+from lmtrex.flask_app.broker.badge import BadgeSvc
 from lmtrex.flask_app.broker.occ import OccurrenceSvc
 
 # bp = Blueprint('occ', __name__, url_prefix='/occ')
@@ -20,6 +22,37 @@ def hello():
 @app.route("/name/<name>")
 def name(name):
     return f"Hello, {escape(name)}!"
+# .....................................................................................
+@app.route("/api/v1/address")
+def address_endpoint():
+    response = AddressSvc.get_endpoint()
+    return response
+
+# .....................................................................................
+@app.route("/api/v1/badge")
+def badge_endpoint():
+    response = BadgeSvc.get_endpoint()
+    return response
+
+# .....................................................................................
+@app.route('/api/v1/badge/<string:provider>', methods=['GET'])
+def badge_get(provider):
+    """Get an occurrence record from available providers.
+
+    Args:
+        provider (str): An provider code for which to return an icon.
+
+    Returns:
+        dict: An image file as binary or an attachment.
+    """
+    # response = OccurrenceSvc.get_occurrence_records(occid='identifier')
+    # provider = request.args.get('provider', default = None, type = str)
+    icon_status = request.args.get('icon_status', default = 'active', type = str)
+    stream = request.args.get('stream', default = 'True', type = str)
+    response = BadgeSvc.get_icon(
+        provider=provider, icon_status=icon_status, stream=stream, app_path=app.root_path)
+    return response
+
 
 # .....................................................................................
 @app.route("/api/v1/occ")
